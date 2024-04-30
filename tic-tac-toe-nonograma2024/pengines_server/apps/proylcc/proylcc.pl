38414ce35eba38f6f6a21dc73eb6381e2ed6595e
anapaulanegrin <anapaulanegrin26@gmail.com>
2024-04-30 20:00:43 -0300

    Avance de prolog

--- tic-tac-toe-nonograma2024/pengines_server/apps/proylcc/proylcc.pl
+++ tic-tac-toe-nonograma2024/pengines_server/apps/proylcc/proylcc.pl
@@ -23,9 +23,10 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
 % put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
 %
 
-put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 0):-
+put(Content, [RowN, ColN], RowsClues, ColsClues, Grid, NewGrid, RowSat, ColSat):-
 	% NewGrid is the result of replacing the row Row in position RowN of Grid by a new row NewRow (not yet instantiated).
 	replace(Row, RowN, NewRow, Grid, NewGrid),
+
 
 	% NewRow is the result of replacing the cell Cell in position ColN of Row by _,
 	% if Cell matches Content (Cell is instantiated in the call to replace/5).	
@@ -34,4 +35,75 @@ put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 0):-
 	(replace(Cell, ColN, _, Row, NewRow),
 	Cell == Content
 		;
-	replace(_Cell, ColN, Content, Row, NewRow)).
+	replace(_Cell, ColN, Content, Row, NewRow)),
+
+	verificarPistaLinea(RowN,RowClues,NewGrid,RowSat),
+	crearLineaColumna(ColN,NewGrid,Linea),
+	verificarPistaLinea(ColN,ColClues,NewGrid,ColSat).
+
+
+crearLineaColumna(ColN,NewGrid,Linea):-
+	Contador is 0,
+	proper_length(NewGrid,Longitud),
+	agregarElemento(Contador,NewGrid,ColN,Longitud,[],Linea)
+	
+
+.
+
+
+agregarElemento(Contador,NewGrid,ColN,Longitud,ListaAux,Lista):-
+	Contador<Longitud-1,
+	nth0(Contador,NewGrid,Fila),
+	nth0(ColN,Fila,Elemento),
+	insertarAlFinal(Elemento,ListaAux,ListaNueva),
+	NuevoContador is Contador +1,
+	agregarElemento(NuevoContador,NewGrid,ColN,Longitud,ListaNueva,Lista);
+	Contador is Longitud-1,
+	nth0(Contador,NewGrid,Fila),
+	nth0(ColN,Fila,Elemento),
+	insertarAlFinal(Elemento,ListaAux,Lista)
+.
+
+verificarPistaLinea(RowN,RowClues,NewGrid,RowSat):-
+	nth0(RowN,NewGrid,Fila),
+	recorrerFila(Fila,[],Lista,0),
+	nth0(RowN,RowClues,PistaFila),
+	satisfaceFila(RowSat,Lista,PistaFila).
+	
+satisfaceFila(RowSat,Lista,PistaFila):-
+	Lista=PistaFila, RowSat=1; 
+	Lista\=PistaFila, RowSat=0.
+
+recorrerFila(['#'|[]],ListaAux,Lista,Contador):-
+    NuevoContador is Contador+1,
+	insertarAlFinal(NuevoContador,ListaAux,Lista).
+
+recorrerFila(['X'|[]],ListaAux,Lista,Contador):-
+    Contador>0,
+	insertarAlFinal(Contador,ListaAux,Lista).
+
+recorrerFila(['_'|[]],ListaAux,Lista,Contador):-
+    Contador>0,
+	insertarAlFinal(Contador,ListaAux,Lista).
+
+recorrerFila(['X'|Xs],ListaAux,Lista, Contador):-
+	Contador>0,
+	insertarAlFinal(Contador,ListaAux,ListaNueva),
+	recorrerFila(Xs,ListaNueva,Lista,0).
+
+recorrerFila(['_'|Xs],ListaAux,Lista,Contador):-
+	Contador>0,
+	insertarAlFinal(Contador,ListaAux,ListaNueva),
+	recorrerFila(Xs,ListaNueva,Lista,0).
+
+recorrerFila(['#'|Xs],ListaAux,Lista,Contador):-
+	NuevoContador is Contador+1,
+	recorrerFila(Xs,ListaAux,Lista,NuevoContador).
+
+insertarAlFinal(E,[],[E]).
+insertarAlFinal(E,[X|L1],[X|L2]):-insertarAlFinal(E,L1,L2).	
+
+
+
+
+
--- tic-tac-toe-nonograma2024/src/Game.js
+++ tic-tac-toe-nonograma2024/src/Game.js
@@ -19,21 +19,11 @@ function Game() {
 
   const togglePaintingMode = () => {
     setIsPaintingMode(prevState => !prevState);
-    if(modo==="Modo cruz") setModo("Modo pintar");
-    if(modo==="Modo pintar") setModo("Modo cruz");
+    if(modo==="Modo cruz") {setModo("Modo pintar"); setElemento('#');}
+    if(modo==="Modo pintar"){ setModo("Modo cruz"); setElemento('X');}
   };
 
-  const cambiarElementoX = () => {
-    setElemento('X');
-  }
-
-  const cambiarElemento_ = () => {
-    setElemento('_');
-  }
-
-  const cambiarElementoPintado = () => {
-    setElemento('#');
-  }
+  
 
   useEffect(() => {
     // Creation of the pengine server instance.    
@@ -59,28 +49,13 @@ function Game() {
     if (waiting) {
       return;
     }
+
     // Build Prolog query to make a move and get the new satisfacion status of the relevant clues.    
     const squaresS = JSON.stringify(grid).replaceAll('"_"', '_'); // Remove quotes for variables. squares = [["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]]
-    if(grid[i][j]==='X') setElemento('X');
-    if(grid[i][j]==='#') setElemento('#');
-    if(grid[i][j]==='_') setElemento('_');
+  
     
-    if(isPaintingMode){
-      if(elemento=== 'X' || elemento==='_'){
-        cambiarElementoPintado();
-      }
-      else{
-        cambiarElemento_();
-      }
-    }
-    else{
-      if(elemento=== '#' || elemento=== '_'){
-        cambiarElementoX();
-      }
-      else{
-        cambiarElemento_();
-      }
-    }
+    
+    
     const rowsCluesS = JSON.stringify(rowsClues);
     const colsCluesS = JSON.stringify(colsClues);
     const queryS = `put("${elemento}", [${i},${j}], ${rowsCluesS}, ${colsCluesS}, ${squaresS}, ResGrid, RowSat, ColSat)`; // queryS = put("#",[0,1],[], [],[["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]], GrillaRes, FilaSat, ColSat)
@@ -108,6 +83,7 @@ function Game() {
         rowsClues={rowsClues}
         colsClues={colsClues}
         onClick={(i, j) => handleClick(i, j)}
+
       />
       <div className="game-info">
         {statusText}
